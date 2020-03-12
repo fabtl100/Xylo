e2e1d1414205aa6446ab94b66f0ffb9d4f93d141
Fabian Gandarilla <fgandarilla@uabc.edu.mx>
2020-03-12 13:47:32 -0600

    Initial commit. Project has a form with buttons to start a song, finish a song/ listen to it, and send the song to the Arduino through the serial port. The notes can be played with the keyboard: from the 'A' key to the 'L' key. The space key can be used to terminate the song. There is a label at the top of the form that shows the sequence being played with the delays.

---
+++ Xylo/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Xylo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.buttonTerminate = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelSequenceIndicator = new System.Windows.Forms.Label();
+            this.labelSequence = new System.Windows.Forms.Label();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(186, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(38, 108);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ab";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(230, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(38, 108);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Bb";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(274, 380);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(38, 108);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "C";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(318, 380);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(38, 108);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Db";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(362, 380);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(38, 108);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Eb";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(406, 380);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(38, 108);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "F";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(450, 380);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(38, 108);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "G";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(494, 380);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(38, 108);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Ab";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Enabled = false;
+            this.button9.Location = new System.Drawing.Point(538, 380);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(38, 108);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Bb";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // buttonTerminate
+            // 
+            this.buttonTerminate.Location = new System.Drawing.Point(601, 423);
+            this.buttonTerminate.Name = "buttonTerminate";
+            this.buttonTerminate.Size = new System.Drawing.Size(129, 23);
+            this.buttonTerminate.TabIndex = 9;
+            this.buttonTerminate.Text = "Terminar / Escuchar";
+            this.buttonTerminate.UseVisualStyleBackColor = true;
+            this.buttonTerminate.Click += new System.EventHandler(this.buttonTerminate_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(601, 380);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(129, 23);
+            this.buttonStart.TabIndex = 10;
+            this.buttonStart.Text = "Comenzar de nuevo";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelSequenceIndicator
+            // 
+            this.labelSequenceIndicator.AutoSize = true;
+            this.labelSequenceIndicator.Location = new System.Drawing.Point(184, 7);
+            this.labelSequenceIndicator.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelSequenceIndicator.Name = "labelSequenceIndicator";
+            this.labelSequenceIndicator.Size = new System.Drawing.Size(61, 13);
+            this.labelSequenceIndicator.TabIndex = 11;
+            this.labelSequenceIndicator.Text = "Secuencia:";
+            // 
+            // labelSequence
+            // 
+            this.labelSequence.AutoSize = true;
+            this.labelSequence.Location = new System.Drawing.Point(184, 31);
+            this.labelSequence.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelSequence.Name = "labelSequence";
+            this.labelSequence.Size = new System.Drawing.Size(0, 13);
+            this.labelSequence.TabIndex = 12;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Enabled = false;
+            this.buttonSend.Location = new System.Drawing.Point(601, 464);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(129, 23);
+            this.buttonSend.TabIndex = 13;
+            this.buttonSend.Text = "Enviar a Arduino";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 499);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.labelSequence);
+            this.Controls.Add(this.labelSequenceIndicator);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonTerminate);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button buttonTerminate;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelSequenceIndicator;
+        private System.Windows.Forms.Label labelSequence;
+        private System.Windows.Forms.Button buttonSend;
+    }
+}
+
---
+++ Xylo/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+using System.Threading;
+
+
+namespace Xylo
+{
+    public partial class Form1 : Form
+    {
+        // song will hold the arrays holding the note and the duration of the note
+        List<int[]> song;
+        // projectDirectory is the path in the computer where the sounds for each note are saved
+        string projectDirectory;
+        // Determines wether the user is starting to play a song or has started already (this is to start the timer the first time)
+        bool isFirstStroke;
+        // time saves the time in ms counted by the timer between strokes
+        int time;
+        // Counter used to jump the line in the label that saves the played notes
+        int jumper;
+        // note holds the played note at a certain time
+        int note;
+        // 
+        System.IO.Ports.SerialPort arduinoPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            // project directory is needed to access the sounds for each note
+            projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;            
+            // Configurate the connection through the serial port to the Arduino
+            arduinoPort          = new System.IO.Ports.SerialPort();
+            arduinoPort.PortName = "COM";
+            arduinoPort.BaudRate = 9600;
+            // Connect to the Arduino
+            //arduinoPort.Open();
+            // Initialize some variables
+            song             = new List<int[]>();
+            isFirstStroke    = true;
+            jumper           = 0;
+            time             = 0;
+            note             = 0;
+        }
+
+        private void button1Press()
+        {
+            SoundPlayer player = new SoundPlayer($"{projectDirectory}\\1.wav");
+            player.Play();
+            labelSequence.Text += "{Ab";
+        }
+
+        private void button2Press()
+        {
+            SoundPlayer player = new SoundPlayer($"{projectDirectory}\\2.wav");
+            player.Play();
+            labelSequence.Text += "{Bb";
+        }
+
+        private void button3Press()
+        {
+            SoundPlayer player = new SoundPlayer($"{projectDirectory}\\3.wav");
+            player.Play();
+            labelSequence.Text += "{C";
+        }
+
+        private void button4Press()
+        {
+            SoundPlayer player = new SoundPlayer($"{projectDirectory}\\4.wav");
+            player.Play();
+            labelSequence.Text += "{Db";
+        }
+
+        private void button5Press()
+        {
+            SoundPlayer player = new SoundPlayer($"{projectDirectory}\\5.wav");
+            player.Play();
+            labelSequence.Text += "{Eb";
+        }
+
+        private void button6Press()
+        {
+            SoundPlayer player = new SoundPlayer($"{projectDirectory}\\6.wav");
+            player.Play();
+            labelSequence.Text += "{F";
+        }
+
+        private void button7Press()
+        {
+            SoundPlayer player = new SoundPlayer($"{projectDirectory}\\7.wav");
+            player.Play();
+            labelSequence.Text += "{G";
+        }
+
+        private void button8Press()
+        {
+            SoundPlayer player = new SoundPlayer($"{projectDirectory}\\8.wav");
+            player.Play();
+            labelSequence.Text += "{Ab";
+        }
+
+        private void button9Press()
+        {
+            SoundPlayer player = new SoundPlayer($"{projectDirectory}\\9.wav");
+            player.Play();
+            labelSequence.Text += "{Bb";
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // Set all vars to their default to start with a new song
+            timer1.Stop();
+            song = new List<int[]>();
+            labelSequence.Text = "";
+            jumper = 0;
+            time = 0;
+            isFirstStroke = true;
+            buttonSend.Enabled = false;
+        }
+
+        private void buttonTerminate_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                song.Add(new int[] { note, time });
+                labelSequence.Text += $",{time}}}]";
+                jumper++;
+                if (jumper == 10)
+                {
+                    labelSequence.Text += "\n";
+                    jumper = 0;
+                }
+                play();
+                time = 0;
+                isFirstStroke = true;
+                buttonSend.Enabled = true;
+            }
+            else
+            {
+                play();
+            }
+        }
+
+        private async void play()
+        {
+            foreach (int[] noteArr in song)
+            {
+                SoundPlayer player = new SoundPlayer($"{projectDirectory}\\{noteArr[0]}.wav");
+                player.Play();
+                await Task.Delay(noteArr[1]*14);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (isFirstStroke)
+            {
+                // Start the timer on the first note
+                timer1.Start();
+                isFirstStroke = false;
+                labelSequence.Text += "[";
+            }
+            else
+            {
+                timer1.Stop();
+                // Add the note with its delay to the song list
+                song.Add(new int[] { note, time });
+                // Add the note with its delay to the label showing the sequence
+                labelSequence.Text += $",{time}}}, ";
+                jumper++;
+                // After then notes a line jump is performed so sequence can continue 
+                if (jumper == 10)
+                {
+                    labelSequence.Text += "\n";
+                    jumper = 0;
+                }
+                time = 0;
+                timer1.Start();
+            }
+            switch (e.KeyCode)
+            {
+                case Keys.A: button1Press(); note = 1; break;
+                case Keys.S: button2Press(); note = 2; break;
+                case Keys.D: button3Press(); note = 3; break;
+                case Keys.F: button4Press(); note = 4; break;
+                case Keys.G: button5Press(); note = 5; break;
+                case Keys.H: button6Press(); note = 6; break;
+                case Keys.J: button7Press(); note = 7; break;
+                case Keys.K: button8Press(); note = 8; break;
+                case Keys.L: button9Press(); note = 9; break;
+                case Keys.Space: buttonTerminate.PerformClick(); break;
+            }
+        }
+
+        // Increment time delay variable every millisecond the timer is enabled
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time++;
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            byte[,] byteArr = new byte[song.Count, 2];
+            byte noteByte;
+            byte delayByte;
+            int i = 0;
+            foreach (var noteArr in song)
+            {
+                noteByte = Convert.ToByte(noteArr[0]);
+                delayByte = Convert.ToByte(noteArr[1]);
+                byteArr[i, 0] = noteByte;
+                byteArr[i, 1] = delayByte;
+                MessageBox.Show($"{byteArr[i, 0]}, {byteArr[i, 1]}");
+            }
+        }
+    }
+}
